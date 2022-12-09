87ffa23567da2d925a93609dd29fd63bbc3eede8
SamuelNdreu <samwellsoficcial@gmail.com>
2022-12-09 15:06:33 +0100

    created repository giving shape to the array

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Te Qift Nusja",
+            "image": "https://unsplash.com/photos/S_YOuAUMm2o"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "E kam fillu nga 0 dhe tani bera Miliona",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Mercedes Benz_Al",
+            "image": "https://unsplash.com/photos/xZIJqYNIOfc"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.com/photos/ELaOukUlgeY",
+        "author": {
+            "name": "Zhelgyre Mistri",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Sikur te kishte halla koqe behesh xhaxha Elvis Doda Oi OI oi. Dum DUm dummy",
+        "media": "https://unsplash.com/photos/IXV_wQLoqbc",
+        "author": {
+            "name": "Aqif Kopertoni",
+            "image": "https://unsplash.com/photos/ud0twV98uvg"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+const container = document.getElementById('container')
