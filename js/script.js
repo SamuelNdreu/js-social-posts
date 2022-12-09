586383804215708a692760e88d44cda5693ad513
SamuelNdreu <samwellsoficcial@gmail.com>
2022-12-09 15:16:07 +0100

    imported js script with container now implementing like buttons

--- js/script.js
+++ js/script.js
@@ -59,3 +59,38 @@ const posts = [
 const container = document.getElementById('container')
 
 for (let i = 0; i < posts.length; i++) {
+    container.innerHTML += 
+    `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${posts[i].author.name}</div>
+                <div class="post-meta__time">${posts[i].created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${posts[i].content}</div>
+    <div class="post__image">
+        <img src="${posts[i].media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+    </div>
+
+    `
+}
